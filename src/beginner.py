0e8ed912932016752ccc73dce0cddfee3392316c
Dragoon8192 <dragoon8192@gmail.com>
2022-10-20 15:30:14 +0900

    update:   src/beginner.py
    Remove type helps.

--- src/beginner.py
+++ src/beginner.py
@@ -1,18 +1,11 @@
 import tensorflow as tf
 import numpy as np
-from nptyping import NDArray, Shape, UInt8, Float, Float32, assert_isinstance
 
 def main() -> None:
 
-    x_train_int :   NDArray[Shape['60000, 28, 28'], UInt8]
-    y_train :       NDArray[Shape['60000'], UInt8]
-    x_test_int :    NDArray[Shape['10000, 28, 28'], UInt8]
-    y_test :        NDArray[Shape['10000'], UInt8]
     mnist = tf.keras.datasets.mnist
     ( x_train_int, y_train ), ( x_test_int, y_test ) = mnist.load_data()
 
-    x_train :   NDArray[Shape['60000, 28, 28'], Float]
-    x_test :    NDArray[Shape['10000, 28, 28'], Float]
     x_train = x_train_int / 255.0
     x_test  = x_test_int / 255.0
 
@@ -23,7 +16,6 @@ def main() -> None:
         tf.keras.layers.Dense( 10 )
     ])
 
-    predications : NDArray[Shape['1, 10'], Float32]
     predications = model( x_train[:1] ).numpy()
     print( 'predications = {0}'.format( predications ) )
     print( 'tf.nn.softmax(predications).numpy() = {0}'.format( tf.nn.softmax(predications).numpy() ) )
